f69f338c1acce304f57fb53277189480cabf6c70
guilhermelps <guilhermelopes17@hotmail.com>
2021-03-08 13:34:28 -0300

    aula 04 concluida

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
+
+import { ChallengeContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengeContext);
+  
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/56078026?s=460&u=5e6e3146d4813fcc0603c49dd5b3eacfbf0275f1&v=4" alt="Guilherme Lopes"/>
@@ -8,7 +14,7 @@ export function Profile() {
         <strong>Guilherme Lopes</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
